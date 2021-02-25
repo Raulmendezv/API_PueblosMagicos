761843cd60735ddfed59407c7f2be3bd49e52bb0
Raulmendezv <a01734429@itesm.mx>
2021-02-25 08:56:29 -0600

    [Add] Admin Controller Template

---
+++ controllers/admin.js
@@ -0,0 +1,41 @@
+const Admin = require('../models/Admin')
+
+function addTown() {
+    // Code
+}
+
+function getAllTowns() {
+    // Code
+}
+
+function getTownBy() {
+    // Code
+}
+
+function updateTown() {
+    // Code
+}
+
+function getAllReviews() {
+    // Code
+}
+
+function getReviewsBy() {
+    // Code
+}
+
+function deleteReview() {
+    // Code
+}
+
+function getAllUsers() {
+    // Code
+}
+
+function getUserBy() {
+    // Code
+}
+
+function deleteUser() {
+    // Code
+}
