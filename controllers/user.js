2443ce883ca238531b33ff379596b0b373b97498
Grufty295 <sebastian.audetat.f@gmail.com>
2021-02-26 01:44:37 -0700

    [Add] User Controller Template

---
+++ controllers/user.js
@@ -0,0 +1,41 @@
+const User = require("../models/User");
+
+function getAllTowns() {
+  // Code
+}
+
+function getTownBy() {
+  // Code
+}
+
+function updateTown() {
+  // Code
+}
+
+function addTownToFavorites() {
+  // Code
+}
+
+function deleteTownFromFavorites() {
+  // Code
+}
+
+function addReview() {
+  // Code
+}
+
+function updateReview() {
+  // Code
+}
+
+function getAllReviews() {
+  // Code
+}
+
+function getReviewsBy() {
+  // Code
+}
+
+function getUsersBy() {
+  // Code
+}
